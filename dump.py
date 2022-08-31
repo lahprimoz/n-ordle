e88dc94757596759783de3bb79ba53f99a4b56b4
lahprimoz <primoz.lah02@gmail.com>
2022-08-31 20:11:48 +0200

    Uredil sprejemljive besede (nove datoteke)

--- dump.py
+++ dump.py
@@ -1,11 +1,33 @@
-import json
+#slovar = {}
+#
+#with open("besede_sprejemljive.txt") as dat:
+#    i = 0
+#    for beseda in dat:
+#        slovar[beseda[:-1]] = len(beseda[:-1])
+#
+#
+#for beseda in slovar:
+#    ime = "sprejemljive dolzina " + str(len(beseda)) 
+#    with open(ime, "a") as datoteka:
+#        datoteka.write(beseda)
+#
+#print("done")
 
-slovar = {}
 
-with open("besede_uradne.txt") as dat:
-    i = 0
+seznam = []
+
+with open("besede_sprejemljive.txt") as dat:
     for beseda in dat:
-        slovar[beseda[:-1]] = len(beseda[:-1])
+        seznam.append(beseda[:-1])
 
-with open("besede_uradne.json", "w") as datoteka:
-    json.dump(slovar, datoteka, indent=4)
+for beseda in seznam:
+    i = 0
+    ime = "sprejemljive dolzine " + str(len(beseda))  + ".txt"
+    with open(ime, "a") as datoteka:
+        print(beseda)
+        datoteka.write(beseda + "\n")
+        if i % 10000 == 0:
+            print(beseda)
+        i += 1
+
+print("done")
